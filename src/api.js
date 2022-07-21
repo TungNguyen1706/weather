785c49f9eb5a2d781dc036d676c2df0bf3209175
TungNguyen1706 <tung.nguyen@relationshop.com>
2022-07-21 16:12:30 +0700

    Initial commit

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "0a37eabademsh1c88a512fc054a4p10404cjsna032f99a2806",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "a38876f9b6b0a3f8b046226d0a9532de";
